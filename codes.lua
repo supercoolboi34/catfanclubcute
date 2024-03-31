d7132cf2bf66004ea10cf5cdc6787a2897dd93a7
epoiw <110830009+epoiw@users.noreply.github.com>
2024-03-31 01:50:33 +0100

    Update codes.lua

--- codes.lua
+++ codes.lua
@@ -6,14 +6,3 @@
             "SORRYFORTHEWAITTWO",
             "ENYU",
         }
-
-        local player = game:GetService("Players").LocalPlayer
-        local sylasGui = player.PlayerGui.SylasGui
-        local codeBox = sylasGui.CodesFrame.MainFrame.CodesFrame.CodesFrameBack.CodesFrame.CodeBox
-
-        for _, code in ipairs(codes) do
-            codeBox.Text = code
-            wait(0.1)
-        end
-    end
-})
